22f7b0f5ef4bf8a8ced3015ba7e7a8d059555249
Stephen Tsoi-A-Sue <stsoiasue@gmail.com>
2018-02-11 15:45:34 -0800

    adjust for Level 2 solution (multi criteria filters)

--- 14-JavaScript/dataGrab.js
+++ 14-JavaScript/dataGrab.js
@@ -6,35 +6,47 @@ function grabData(event) {
     d3.event.preventDefault();
     // remove previous query results
     d3.selectAll(".sightingsRow").remove()
-
+    
     // retrieve query date from input box
-    var $dateSearch = d3.select('#dateSearchValue').node().value;
-    console.log('user is looking for sightings on: ' + $dateSearch)
+    var queryData = {
+        datetime: d3.select('#dateSearchValue').node().value,
+        city: d3.select('#citySearchValue').node().value,
+        state: d3.select('#stateSearchValue').node().value,
+        country: d3.select('#countrySearchValue').node().value,
+        shape: d3.select('#shapeSearchValue').node().value,
+    };
+    
+    // change origial dataset to avoid mutation
+    var filteredData = dataSet.concat();
+
+    // loop through query data points
+    for (var key in queryData) {
+        filteredData = filteredData.filter (function(data) {
+            if (queryData[key] === "" || queryData[key] === data[key]) {
+                return true;
+            };
+            return false;
+        });
+    };
+
+    // return filter results
+    console.log(filteredData);
 
     // define variable to count number of values returned
-    sightingsCount = 0
+    console.log('there were ' + filteredData.length + ' sightings that fit the filter critera.')
 
     // loop through sightings 
-    dataSet.forEach( function(sighting) {
+    filteredData.forEach( function(sighting) {
+        // append row to table element on html file
+        var $tr = d3.select("#sightingsTable").append("tr").attr("class", "sightingsRow");
 
-        // verify date matches query date
-        if (sighting.datetime === $dateSearch) {
-            
-            // append row to table element on html file
-            var $tr = d3.select("#sightingsTable").append("tr").attr("class", "sightingsRow");
-
-            // append table data
-            $tr.append("td").text(sighting.datetime);
-            $tr.append("td").text(sighting.city);
-            $tr.append("td").text(sighting.state);
-            $tr.append("td").text(sighting.country);
-            $tr.append("td").text(sighting.shape);
-
-            sightingsCount++;
-        };  
+        // append table data
+        $tr.append("td").text(sighting.datetime);
+        $tr.append("td").text(sighting.city);
+        $tr.append("td").text(sighting.state);
+        $tr.append("td").text(sighting.country);
+        $tr.append("td").text(sighting.shape);
     });
-    console.log('there were ' + sightingsCount + ' sightings on ' + $dateSearch)
 };
 
 d3.select("#submit").on("click", grabData);
-
